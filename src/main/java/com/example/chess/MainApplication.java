6d5717e509459c49ab5750ac8acd1a89ab668cb7
Tom Gallagher <tga62@uclive.ac.nz>
2024-04-03 15:53:28 +1300

    deleted example controller and changed application file name

--- src/main/java/com/example/chess/MainApplication.java
+++ src/main/java/com/example/chess/MainApplication.java
@@ -1,23 +1,12 @@
 package com.example.chess;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.stage.Stage;
-import com.example.chess.BoardManager;
 
-public class HelloApplication extends Application {
+public class MainApplication extends Application {
     public static boolean PLAY_AS_WHITE = true;
     public static boolean PLAY_AS_BLACK = false;
     GridPane root = new GridPane();
